fe42a2e26649d57837452b8d4d4de89128dc42d3
Jiali Chen <jialche@microsoft.com>
2017-09-21 13:19:27 -0700

    minor fix

--- teams-react-component/src/anchor/index.tsx
+++ teams-react-component/src/anchor/index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
+import { classes } from 'typestyle';
 import { connectTeamsComponent, InjectedTeamsProps } from '../index';
 
 export interface AnchorProps
   extends React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> { }
 
 const AnchorInternal: React.StatelessComponent<AnchorProps & InjectedTeamsProps> = (props) => {
-  const { theme, ...rest } = props;
+  const { theme, className, ...rest } = props;
   return (
-    <a className={theme.anchor} {...rest}>{props.children}</a>
+    <a className={classes(theme.anchor, className)} {...rest}>{props.children}</a>
   );
 };
 
