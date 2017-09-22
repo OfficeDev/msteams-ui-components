5b343c0776daecac4302e705d8b083580ed054c4
Matt Stankiewicz <mastank@microsoft.com>
2017-09-22 16:51:43 -0700

    Move some typestyle bindings around

--- msteams-ui-styles-core/src/typestyle-binding/panels/panel.ts
+++
@@ -1,7 +0,0 @@
-import { style } from 'typestyle';
-import { Context } from '../../raw/context';
-import { panel as panelRaw } from '../../raw/panels';
-
-export function panel(c: Context) {
-  return style(panelRaw(c).normal);
-}
