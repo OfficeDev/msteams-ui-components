ba824bcdcc6f22a75cb966f99ae0c963b7c1d84c
Jiali Chen <jialche@microsoft.com>
2017-10-20 12:28:23 -0700

    improve API

--- msteams-ui-components-react/src/table/tr.tsx
+++ msteams-ui-components-react/src/table/tr.tsx
@@ -1,10 +1,15 @@
 import { table } from 'msteams-ui-styles-core/lib/components/table';
 import * as React from 'react';
 import { connectTeamsComponent, InjectedTeamsProps } from '../index';
+import classes from '../utils/classes';
 
-const TrInternal: React.StatelessComponent<InjectedTeamsProps> = (props) => {
-  const themeClassNames = table(props.context);
-  return <tr className={themeClassNames.tr}>{props.children}</tr>;
+export interface TrProps
+  extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLTableRowElement>, HTMLTableRowElement> { }
+
+const TrInternal: React.StatelessComponent<TrProps & InjectedTeamsProps> = (props) => {
+  const { context, className, ...rest } = props;
+  const themeClassNames = table(context);
+  return <tr className={classes(themeClassNames.tr, className)} {...rest}>{props.children}</tr>;
 };
 
-export const Tr = connectTeamsComponent<{}>(TrInternal);
+export const Tr = connectTeamsComponent<TrProps>(TrInternal);
