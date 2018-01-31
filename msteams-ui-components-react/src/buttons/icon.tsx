296ac5f850321d8d560215938b82502fb0767a85
Matt Stankiewicz <mastank@microsoft.com>
2018-01-31 15:57:31 -0800

    Add icon buttons to the control library

---
+++ msteams-ui-components-react/src/buttons/icon.tsx
@@ -0,0 +1,54 @@
+import { iconButton } from 'msteams-ui-styles-core/lib/components/icon-button';
+import * as React from 'react';
+import { MSTeamsIcon, MSTeamsIconType, MSTeamsIconWeight } from '../../../msteams-ui-icons/react/lib/index';
+import { Focusable } from '../focusable';
+import { InjectedTeamsProps } from '../index';
+import { connectTeamsComponent } from '../teams-context';
+import classes from '../utils/classes';
+
+export interface IconButtonProps
+  extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  onRef?: (ref: React.Component & Focusable | null) => void;
+  iconType: string;
+  iconWeight?: number;
+}
+
+class IconButtonView
+  extends React.Component<IconButtonProps & InjectedTeamsProps>
+  implements Focusable {
+  private button: HTMLButtonElement | null;
+
+  componentDidMount() {
+    if (this.props.onRef) {
+      this.props.onRef(this);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.onRef) {
+      this.props.onRef(null);
+    }
+  }
+
+  focus() {
+    if (this.button) {
+      this.button.focus();
+    }
+  }
+
+  render() {
+    const { context, className, iconType, iconWeight, children, onRef, ...rest } = this.props;
+    const themeClassName = iconButton(context);
+
+    return <button
+      ref={(ref) => this.button = ref}
+      role="button"
+      className={classes(themeClassName, className)}
+      {...rest}>
+      <MSTeamsIcon iconType={iconType} iconWeight={iconWeight || MSTeamsIconWeight.Light} />
+      {children}
+    </button>;
+  }
+}
+
+export const IconButton = connectTeamsComponent(IconButtonView);
--- msteams-ui-components-react/src/buttons/index.ts
+++ msteams-ui-components-react/src/buttons/index.ts
@@ -1,3 +1,4 @@
 export * from './primary';
 export * from './secondary';
 export * from './compound';
+export * from './icon';
