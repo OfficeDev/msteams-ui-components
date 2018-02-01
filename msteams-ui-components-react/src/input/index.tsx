375b959918f4d707fbe0c0eaad081a23054681d4
Matt Stankiewicz <mastank@microsoft.com>
2018-02-01 11:27:54 -0800

    Replace two labels with one label to fix aria for edge

--- msteams-ui-components-react/src/input/index.tsx
+++ msteams-ui-components-react/src/input/index.tsx
@@ -53,16 +53,12 @@ implements Focusable {
         style={style}
         className={classes(themeClassNames.container, className)}>
         <label
-          hidden={!!label}
+          aria-live="assertive"
           tabIndex={-1}
-          className={themeClassNames.label}
-          htmlFor={actualId}>{label}</label>
-        <label
-          hidden={!!errorLabel}
-          aria-live="polite"
-          tabIndex={-1}
-          className={themeClassNames.errorLabel}
-          htmlFor={actualId}>{errorLabel}</label>
+          htmlFor={actualId}>
+          <span hidden={!!label} className={themeClassNames.label}>{label}</span>
+          <span hidden={!!errorLabel} className={themeClassNames.errorLabel}>{errorLabel}</span>
+        </label>
         <input
           id={actualId}
           ref={(ref) => this.input = ref}
